0b06516410219cfe8a92f3ef9e319394f4bc8b21
Xavi <ravirubio2621@gmail.com>
2025-01-23 19:31:14 +0100

    Estilos mainLoged

---
+++ vista/mainLoged.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/mainLoged.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="semiTransparentBig">
+        <button class="smallBt">LOGOUT</button>
+        <button class="bigBt">JOIN MATCH</button>
+        <button class="bigBt">VIEW STORE</button>
+    </div>
+</body>
+</html>
